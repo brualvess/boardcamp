0789e68a21e1f45986d9389d32e16253457b54d1
brualvess <oliveirabrunna2210@gmail.com>
2022-07-21 17:44:13 -0300

    create games

---
+++ src/controllers/gamesControllers.js
@@ -0,0 +1,39 @@
+import {connection} from '../dbStrategy/postgres.js'
+import joi from 'joi'
+
+export async function createGames(req,res){
+    const datas = req.body
+    const schemaCategorie = joi.object({
+        name: joi.string().required(),
+        stockTotal:joi.number().greater(0),
+        categoryId: joi.number(),
+        pricePerDay: joi.number().greater(0),
+
+    })
+     const {error} = schemaCategorie.validate(datas)
+    const {rows:verifyIdCategorie} = await connection.query(
+        `SELECT * FROM categories WHERE id = ${datas.categoryId}`
+        )
+    if(error || verifyIdCategorie.length == 0 ){
+        res.sendStatus(400)
+        return
+    }
+    const{rows:verifyNameGame} = await connection.query(
+        `SELECT * FROM games WHERE name = '${datas.name}'`
+    )
+    if(verifyNameGame.length != 0){
+        res.sendStatus(409)
+        return
+    }
+    await connection.query(
+        `INSERT INTO games (name, image, "stockTotal", "categoryId", "pricePerDay") 
+        VALUES ( 
+            '${datas.name}', 
+            '${datas.image}', 
+             ${datas.stockTotal}, 
+             ${datas.categoryId}, 
+             ${datas.pricePerDay}
+            )`
+        )
+   res.sendStatus(201)
+}
--- src/routes.js
+++ src/routes.js
@@ -3,10 +3,12 @@ import {
     createCategories,
     listCategories
 } from './controllers/categoriesControllers.js';
+import { createGames } from './controllers/gamesControllers.js';
 
 const router = Router()
 
 router.post('/categories', createCategories)
 router.get('/categories', listCategories)
+router.post('/games', createGames)
 
 export default router;
