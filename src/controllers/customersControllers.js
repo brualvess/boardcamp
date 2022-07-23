862b6272bdb133e94a97078bea2d385317149b47
brualvess <oliveirabrunna2210@gmail.com>
2022-07-23 10:43:41 -0300

    update customers

--- src/controllers/customersControllers.js
+++ src/controllers/customersControllers.js
@@ -47,7 +47,7 @@ export async function createCustomers(req, res) {
 }
 export async function listCustomers(req, res) {
     const cpf = req.query.cpf
-    if(cpf){
+    if (cpf) {
         const { rows: getCpf } = await connection.query(
             'SELECT * FROM customers WHERE cpf LIKE $1', [cpf + '%']
         )
@@ -73,3 +73,40 @@ export async function listWithId(req, res) {
     }
 
 }
+export async function updateCustomers(req, res) {
+    const datas = req.body
+    const id = parseInt(req.params.id)
+    const schemaCustomers = joi.object({
+        name: joi.string().required(),
+        phone: joi.string().min(10).max(11).pattern(/^[0-9]+$/),
+        cpf: joi.string().length(11).pattern(/^[0-9]+$/),
+        birthday: Joi.date().format('YYYY-MM-DD')
+    })
+    const { error } = schemaCustomers.validate(datas)
+    if (error) {
+        res.sendStatus(400)
+        return
+    }
+    try {
+        const { rows: cpf } = await connection.query(
+            'SELECT * FROM customers WHERE cpf =$1', [datas.cpf]
+        )
+        if (cpf.length != 0) {
+            res.sendStatus(409)
+            return
+        }
+    } catch {
+        res.sendStatus(500)
+        return
+    }
+    await connection.query(
+        `UPDATE customers SET
+         name = $1,
+         phone = $2,
+         cpf = $3,
+         birthday =$4
+         WHERE id = $5`,
+        [datas.name, datas.phone, datas.cpf, datas.birthday, id]
+    )
+    res.sendStatus(200)
+} 
--- src/routes.js
+++ src/routes.js
@@ -9,17 +9,24 @@ import {
      } from './controllers/gamesControllers.js';
 import { createCustomers,
          listCustomers,
-         listWithId
+         listWithId,
+         updateCustomers
 } from './controllers/customersControllers.js';
 
 const router = Router()
 
+// Category routes
 router.post('/categories', createCategories)
 router.get('/categories', listCategories)
+
+// Game routes
 router.post('/games', createGames)
 router.get('/games', listGames)
+
+// Customer routes
 router.post('/customers', createCustomers)
 router.get('/customers', listCustomers)
 router.get('/customers/:id', listWithId)
+router.put('/customers/:id', updateCustomers)
 
 export default router;
